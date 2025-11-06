c19ce980340072d59a7733553be960dcc7f88dd5
Chigemezu Emmanuel <chigemezuemmanuel641@gmail.com>
2025-11-06 23:56:34 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+
+const port = process.env.PORT || 4011;
+const app = express();
+const __dirname = path.resolve();
+
+app.use(express.static('public'));
+
+
+
+
+
+
+app.get('/', (request, response) => {
+
+    const filePath = path.join(__dirname, 'Home.html');
+
+    response.sendFile(filePath);
+
+});
+
+ 
+
+
+
+
+app.listen(port, '0.0.0.0', () => {
+
+    console.log(`Server is running on port ${port}.`);
+
+});
